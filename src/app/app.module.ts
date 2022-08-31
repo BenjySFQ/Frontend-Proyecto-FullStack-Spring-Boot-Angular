129b27d269f63147152a2942c715a386a2d96c2f
BenjySFQ <benjamin77122@gmail.com>
2022-08-31 13:32:02 -0500

    advanced component login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,32 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatButtonModule} from '@angular/material/button';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SignupComponent } from './pages/signup/signup.component';
+
+
 import { LoginComponent } from './pages/login/login.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { SignupComponent } from './pages/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
 
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SignupComponent,
     LoginComponent,
+    HomeComponent,
+    SignupComponent,
+    NavbarComponent
+  
     
   ],
   imports: [
@@ -36,7 +43,10 @@ import { FormsModule } from '@angular/forms';
     MatInputModule,
     HttpClientModule,
     FormsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule
     
     
   ],
